9e03872a35ae3a41cd44ac6c32a66b5d674d5594
BodyaVan <bodya.van@gmail.com>
2015-03-25 17:57:09 +0300

    add subject controller, what handles REST requests

---
+++ src/main/java/ua/web_challenge/volunteer/controller/SubjectController.java
@@ -0,0 +1,78 @@
+package ua.web_challenge.volunteer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import ua.web_challenge.volunteer.entity.Subject;
+import ua.web_challenge.volunteer.persistence.SubjectDao;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+
+/**
+ * Created on 24.03.2015
+ *
+ * @author Bohdan Vanchuhov
+ */
+@Controller
+@RequestMapping("/subjects")
+public class SubjectController {
+    private SubjectDao subjectDao;
+
+    @Autowired
+    public SubjectController(SubjectDao subjectDao) {
+        this.subjectDao = subjectDao;
+    }
+
+    @RequestMapping(value = "/{id}", method = GET, headers = "Accept=application/json")
+    public @ResponseBody Subject getSubject(@PathVariable("id") int id) {
+        Subject subject = subjectDao.findById(id);
+
+        checkSubject(subject);
+
+        return subject;
+    }
+
+    private void checkSubject(Subject subject) {
+        if (subject == null) {
+            throw new ResourceNotFoundException();
+        }
+    }
+
+    @RequestMapping(value = "/{id}", method = PUT, headers = "Content-Type=application/json")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateSubject(@PathVariable("id") int id, @Valid @RequestBody Subject subject) {
+        subjectDao.update(subject);
+    }
+
+    @RequestMapping(value = "/{id}", method = DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteSubject(@PathVariable("id") int id) {
+        subjectDao.delete(id);
+    }
+
+    @RequestMapping(method = POST, headers = "Content-Type=application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public @ResponseBody Subject createSubject(@Valid @RequestBody Subject subject,
+                                               BindingResult bindingResult, HttpServletResponse response)
+            throws BindException {
+        if (bindingResult.hasErrors()) {
+            throw new BindException(bindingResult);
+        }
+
+        subjectDao.add(subject);
+
+        setCreatedSubjectLocation(subject, response);
+
+        return subject;
+    }
+
+    private void setCreatedSubjectLocation(Subject subject, HttpServletResponse response) {
+        response.setHeader("Location", "/subjects/" + subject.getId());
+    }
+}
---
+++ src/test/java/ua/web_challenge/volunteer/controller/SubjectControllerTest.java
@@ -0,0 +1,101 @@
+package ua.web_challenge.volunteer.controller;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.ExpectedDatabase;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static com.github.springtestdbunit.assertion.DatabaseAssertionMode.NON_STRICT;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+/**
+ * Created on 24.03.2015
+ *
+ * @author Bohdan Vanchuhov
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration({
+        "classpath:/spring/test-application-context.xml"
+})
+@TestExecutionListeners({
+        DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class,
+        DbUnitTestExecutionListener.class})
+@DatabaseSetup("/datasets/subject/subject-data.xml")
+@DatabaseSetup("/datasets/subject/subject-data.xml")
+public class SubjectControllerTest {
+    private MockMvc mockMvc;
+
+    @SuppressWarnings("SpringJavaAutowiringInspection")
+    @Autowired
+    protected WebApplicationContext wac;
+
+    @Before
+    public void setUp() {
+        this.mockMvc = webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void getSubject() throws Exception {
+        mockMvc.perform(get("/subjects/1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.name", is("Education")));
+    }
+
+    @Test
+    public void getSubject_wrongId() throws Exception {
+        mockMvc.perform(get("/subjects/100"))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @ExpectedDatabase(value = "/datasets/subject/subject-data-after-update.xml", assertionMode = NON_STRICT)
+    public void updateCountry() throws Exception {
+        mockMvc.perform(put("/subjects/1")
+                        .content("{\"id\":1, \"name\":\"Education/Teaching\"}")
+                        .contentType(MediaType.APPLICATION_JSON)
+        )
+                .andDo(print())
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    @ExpectedDatabase(value = "/datasets/subject/subject-data-after-delete.xml", assertionMode = NON_STRICT)
+    public void deleteSubject() throws Exception {
+        mockMvc.perform(delete("/subjects/1"))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    @ExpectedDatabase(value = "/datasets/subject/subject-data-after-insert.xml", assertionMode = NON_STRICT)
+    public void createSubject() throws Exception {
+        mockMvc.perform(post("/subjects")
+                        .content("{\"name\":\"Community\"}")
+                        .contentType(MediaType.APPLICATION_JSON)
+        )
+                .andDo(print())
+                .andExpect(status().isCreated());
+    }
+}
